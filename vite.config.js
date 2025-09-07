e7f01bfd0596e75f7775a65f18f3e1a7d9fd03c8
José Catalán <jcatalansolis@gmail.com>
2025-09-07 20:15:17 -0300

    instale gh -pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/desafio_pizzeria_mamamia/',
+  base: '/pizzer-a_mamam-a/',
 })
